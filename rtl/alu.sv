7502de824c51bf79b4598521fe4a075b5e922a2b
Michael J Kirchhofer <mjki@kth.se>
2025-09-16 15:32:40 +0200

    implemented ADD and SUB operations with overflow detection with testbench cases

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -10,6 +10,39 @@ module alu #(
 
   // Complete your RTL code here
 
+
+  logic signed [BW-1:0] a_s,b_s,out_s;
+  logic overflow;
+  assign a_s = in_a;
+  assign b_s = in_b; 
+
+  always_comb begin
+    out_s = '0;
+    overflow   = 1'b0;
+     case (opcode)
+        4'b0000: begin
+          out_s = a_s + b_s; // ADD
+          overflow = ~(a_s[BW-1] ^ b_s[BW-1]) & (a_s[BW-1] ^ b_s[BW-1] ^out_s[BW-1]); // overflow
+        end
+        4'b0001: begin
+          out_s = a_s - b_s; // SUB
+          overflow = (a_s[BW-1] ^ b_s[BW-1]) & (a_s[BW-1] ^ out_s[BW-1]); // overflow
+        end
+        4'b0010: out_s = in_a & in_b; // AND
+        4'b0011: out_s = in_a | in_b; // OR
+        4'b0100: out_s = in_a ^ in_b; // XOR
+        4'b0101: out_s = in_a + 1;    // INC
+        4'b0110: out_s = in_a;   // PASS A
+        4'b0111: out_s = in_b;   // PASS B
+        default: out_s = '0;           // NOP or undefined operation
+      
+     endcase
+  end
+  
+  assign out = out_s;
+  assign flags[2] = overflow; // overflow
+  assign flags[1] = out_s[BW-1]; // negative
+  assign flags[0] = (out_s == '0) ? 1'b1 : 1'b0; // zero
 endmodule
 
 
--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -23,6 +23,20 @@ module alu_tb;
     in_b = '0;
     opcode = '0;
     #10ns;
+    in_a = 16'h0002; in_b = 16'h0002; opcode = 4'b0000; // ADD
+    #10ns;
+    in_a = 16'hFFFF; in_b = 16'hFFFF;// ADD
+    #10ns;
+    in_a = 16'h0005; in_b = 16'h0003; opcode = 4'b0001; // SUB
+    #10ns;
+    in_a = 16'h0002; in_b = 16'h0003; // SUB
+    #10ns;
+    in_a = 16'h7FFF; in_b = 16'hFFFF; // 32767 - (-1) = overflow to -32768
+    #10ns;
+    in_a = 16'h7FFF; in_b = 16'h8000; // 32767 - (-32768) = overflow to -1
+    #10ns;
+    in_a = 16'h000F; in_b = 16'h000F; // Test for Zero flag
+    #10ns;
     // Complete your testbench code here
   end
 endmodule
