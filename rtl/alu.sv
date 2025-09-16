f61e5597cc209724e4d2c278f67139fbabb884e7
Michael <kirchhofermichael@gmail.com>
2025-09-16 17:29:32 +0200

    Implemented logic and testcases for other operators

--- rtl/alu.sv
+++ rtl/alu.sv
@@ -1,5 +1,5 @@
 module alu #(
-  BW = 16 // bitwidth
+  parameter int unsigned BW = 16 // bitwidth
   ) (
   input  logic unsigned [BW-1:0] in_a,
   input  logic unsigned [BW-1:0] in_b,
@@ -13,36 +13,46 @@ module alu #(
 
   logic signed [BW-1:0] a_s,b_s,out_s;
   logic overflow;
-  assign a_s = in_a;
-  assign b_s = in_b; 
+  assign a_s = $signed(in_a);
+  assign b_s = $signed(in_b); 
 
   always_comb begin
     out_s = '0;
     overflow   = 1'b0;
      case (opcode)
-        4'b0000: begin
-          out_s = a_s + b_s; // ADD
+
+        4'b0000: begin // ADD
+          out_s = a_s + b_s; 
           overflow = ~(a_s[BW-1] ^ b_s[BW-1]) & (a_s[BW-1] ^ b_s[BW-1] ^out_s[BW-1]); // overflow
         end
-        4'b0001: begin
-          out_s = a_s - b_s; // SUB
+
+        4'b0001: begin // SUB
+          out_s = a_s - b_s; 
           overflow = (a_s[BW-1] ^ b_s[BW-1]) & (a_s[BW-1] ^ out_s[BW-1]); // overflow
         end
         4'b0010: out_s = in_a & in_b; // AND
         4'b0011: out_s = in_a | in_b; // OR
         4'b0100: out_s = in_a ^ in_b; // XOR
-        4'b0101: out_s = in_a + 1;    // INC
+
+        4'b0101: begin // INC
+           out_s = a_s + 1'b1;
+           overflow = (~a_s[BW-1] & out_s[BW-1]); // overflow if a changes sign
+        end   
         4'b0110: out_s = in_a;   // PASS A
         4'b0111: out_s = in_b;   // PASS B
-        default: out_s = '0;           // NOP or undefined operation
-      
+
+        default: begin // default
+          out_s = '0;           
+          overflow = 1'b0;
+        end     
      endcase
   end
   
   assign out = out_s;
-  assign flags[2] = overflow; // overflow
-  assign flags[1] = out_s[BW-1]; // negative
-  assign flags[0] = (out_s == '0) ? 1'b1 : 1'b0; // zero
+  // 3-bit flag output -> overflow, negative, zero's
+  assign flags = {overflow, out_s[BW-1], (out_s == '0)};
+
+
 endmodule
 
 
--- tb/alu_tb.sv
+++ tb/alu_tb.sv
@@ -23,20 +23,65 @@ module alu_tb;
     in_b = '0;
     opcode = '0;
     #10ns;
-    in_a = 16'h0002; in_b = 16'h0002; opcode = 4'b0000; // ADD
-    #10ns;
-    in_a = 16'hFFFF; in_b = 16'hFFFF;// ADD
-    #10ns;
-    in_a = 16'h0005; in_b = 16'h0003; opcode = 4'b0001; // SUB
-    #10ns;
-    in_a = 16'h0002; in_b = 16'h0003; // SUB
-    #10ns;
+    
+    // ------------------- ADD
+    in_a = 16'h0002; in_b = 16'h0002; opcode = 4'b0000;
+    #5ns;
+    in_a = 16'hFFFF; in_b = 16'hFFFF;
+    #5ns;
+    // ------------------- SUB
+    in_a = 16'h0005; in_b = 16'h0003; opcode = 4'b0001; 
+    #5ns;
+    in_a = 16'h0002; in_b = 16'h0003;
+    #5ns;
     in_a = 16'h7FFF; in_b = 16'hFFFF; // 32767 - (-1) = overflow to -32768
-    #10ns;
+    #5ns;
     in_a = 16'h7FFF; in_b = 16'h8000; // 32767 - (-32768) = overflow to -1
-    #10ns;
+    #5ns;
     in_a = 16'h000F; in_b = 16'h000F; // Test for Zero flag
-    #10ns;
+    #5ns;
+    // ------------------- AND
+    in_a = 16'h0010; in_b = 16'h0010; opcode = 4'b0010;
+    #5ns;
+    in_a = 16'h000F; in_b = 16'hF000; 
+    #5ns;
+    in_a = 16'hFFFF; in_b = 16'hFFFF;
+    #5ns;
+
+    // ------------------- OR
+    in_a = 16'h0010; in_b = 16'h0010; opcode = 4'b0011;
+    #5ns;
+    in_a = 16'h000F; in_b = 16'hF000;
+    #5ns;
+    in_a = 16'hFFFF; in_b = 16'hFFFF;
+    #5ns;
+    
+    // ------------------- XOR
+    in_a = 16'h0010; in_b = 16'h0010; opcode = 4'b0100;
+    #5ns;
+    in_a = 16'h000F; in_b = 16'hF000; 
+    #5ns;
+    in_a = 16'hFFFF; in_b = 16'hFFFF;
+    #5ns;
+
+    // ------------------- INC (in_b can stay at old value)
+    in_a = 16'h0001; opcode = 4'b0101;
+    #5ns;
+    in_a = 16'hFFFF;
+    #5ns;
+
+    // ------------------- PASS A (in_b can stay at old value)
+    in_a = 16'h0FF0; opcode = 4'b0110;
+    #5ns;
+
+    // ------------------- PASS B (in_a can stay at old value)
+    in_b = 16'hFFF0; opcode = 4'b0111;
+    #5ns;
+
+    // -------------------DEFAULT
+    opcode = 4'b1111;
+    #5ns;
+    
     // Complete your testbench code here
   end
 endmodule
